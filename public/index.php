94242e9d9109affdf5bddb5353f4225d6826a9b8
Jessica <jessica.manent@gmail.com>
2020-04-23 11:18:11 +0200

    add composer & other

--- public/index.php
+++ public/index.php
@@ -7,17 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500&family=Open+Sans:wght@300;400;600&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500&family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/5bb1d77498.js" crossorigin="anonymous"></script>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 
-    <title>Social Network Name</title>
+    <title>Site name</title>
 </head>
 
 <body>
@@ -26,8 +23,7 @@
     <div class="sticky-top bg-white shadow-sm">
         <div class="container p-0">
             <nav class="navbar navbar-expand-lg py-4 pr-0">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -36,14 +32,14 @@
                         <div class="col-lg-6"></div>
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link font-weight-bold pl-0" href="#"> 🦇 CREEPY CUTE</a>
+                                <a class="nav-link font-weight-bold pl-0" href="#">CREEPY CUTE</a>
                             </li>
                         </ul>
                     </div>
                     <div class="col-lg-6 mr-auto"></div>
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">SIGN IN</a>
+                            <a class="nav-link" href="#">LOG IN</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link btn btn-outline-dark btn-custom" href="#">SIGN UP</a>
@@ -53,7 +49,6 @@
             </nav>
         </div>
     </div>
-
 
     <div class="">
         <!-- HEADER -->
@@ -68,7 +63,6 @@
             </div>
         </div>
     </div>
-
 
     <!-- MAIN -->
     <main class="container bg-light-lilac mb-5">
@@ -87,8 +81,7 @@
             <div class="col-lg-8">
                 <div class="px-5 pt-5 pb-3">
                     <div class="card mb-3">
-                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD"
-                            class="card-img-top" alt="...">
+                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title h5">Card title</h3>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
@@ -99,8 +92,7 @@
                 </div>
                 <div class="px-5 py-3">
                     <div class="card mb-3">
-                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD"
-                            class="card-img-top" alt="...">
+                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title h5">Card title</h3>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
@@ -111,8 +103,7 @@
                 </div>
                 <div class="px-5 py-3">
                     <div class="card mb-3">
-                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD"
-                            class="card-img-top" alt="...">
+                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title h5">Card title</h3>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
@@ -124,8 +115,7 @@
                 </div>
                 <div class="px-5 py-3">
                     <div class="card mb-3">
-                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD"
-                            class="card-img-top" alt="...">
+                        <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/61454988_2222946187752215_4021438923614978048_o.jpg?_nc_cat=100&_nc_sid=8024bb&_nc_ohc=-C9GyrDLeNMAX9QEO_j&_nc_ht=scontent-cdg2-1.xx&oh=79f50e36d00a12c57a258d8d9d8d4ddf&oe=5EC774BD" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3 class="card-title h5">Card title</h3>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
@@ -140,11 +130,10 @@
 
     <!-- FOOTER -->
 
-    <div class="sticky-top bg-lilac">
+    <div class="sticky-top">
         <div class="container p-0">
             <nav class="navbar navbar-expand-lg py-4">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -173,15 +162,9 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
