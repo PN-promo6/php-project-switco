62c57929e8e5063170963faf05f50fd569b0dd81
Jessica Manent <jessica.manent@gmail.com>
2020-06-02 18:02:39 +0200

    add some style

--- templates/LoginForm.php
+++ templates/LoginForm.php
@@ -8,7 +8,6 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500&family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/5bb1d77498.js" crossorigin="anonymous"></script>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -16,7 +15,7 @@
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/login.css">
 
-    <title>Mon super site</title>
+    <title>Welcome back sur Creepy Cute !</title>
 </head>
 
 <body id="bootstrap-overrides-bg">
@@ -33,32 +32,23 @@
             </nav>
         </div>
     </div>
-
-    <!-- <div class="bg-gradient py-5">
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h1 class="m-0 title-font text-white">Discover creepy cute creations from people all around the world</h1>
-                </div>
+    <!-- FORM-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto mt-5">
+                <form class="form-group" method="POST" action="?action=login">
+                    <h2 class="mt-5 mb-4">🦇 Welcome Back !</h2>
+                    <?php
+                    if (isset($errorMsg)) {
+                        echo "<div class='alert alert-warning' role='alert'>$errorMsg</div>";
+                    }
+                    ?>
+                    <input type="text" class="form-control mb-4" name="username" placeholder="Email Address" required="" autofocus="" />
+                    <input type="password" class="form-control mb-4" name="password" placeholder="Password" required="" />
+                    <button class="btn btn-dark btn-block" type="submit">Login</button>
+                </form>
             </div>
         </div>
-    </div> -->
-
-    <div class="row">
-        <div class="col-md-3 mx-auto mt-5">
-            <form class="form-group" method="POST" action="?action=login">
-                <h2 class="mt-5 mb-4">🦇 Welcome Back !</h2>
-                <?php
-                if (isset($errorMsg)) {
-                    echo "<div class='alert alert-warning' role='alert'>$errorMsg</div>";
-                }
-                ?>
-                <input type="text" class="form-control mb-4" name="username" placeholder="Email Address" required="" autofocus="" />
-                <input type="password" class="form-control mb-4" name="password" placeholder="Password" required="" />
-                <button class="btn btn-dark btn-block" type="submit">Login</button>
-            </form>
-        </div>
-    </div>
     </div>
 </body>
 
